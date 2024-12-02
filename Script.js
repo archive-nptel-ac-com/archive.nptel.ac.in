ced664bb954552e647adaf93071cacc2f514f2cd
archive-nptel-ac-com <nirajsinghrajput329@gmail.com>
2024-12-02 19:07:15 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     // Path to the PDF file you want to allow the user to download
-    var pdfUrl = 'https://drive.google.com/uc?export=download&id=1Bw-WzvG7xmvTQsiImCeFOgnrS5RgMrjp'; // Replace with your PDF file's URL
+    var pdfUrl = 'https://www.dropbox.com/scl/fi/evhd6ebuh2n43p4h9aca5/Niraj-Certificate.pdf?rlkey=2sz7ed5b1icpiw3k7j14kjd2y&st=yje9e9v9&dl=1'; // Replace with your PDF file's URL
 
     // Trigger the download
     var link = document.createElement('a');
