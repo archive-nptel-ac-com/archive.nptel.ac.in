3f7f1495a16ed60cd321c24afdf99979f278854b
archive-nptel-ac-com <nirajsinghrajput329@gmail.com>
2024-12-02 21:28:38 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 document.getElementById('downloadBtn').addEventListener('click', function() {
     // Path to the PDF file you want to allow the user to download
-    var pdfUrl = 'https://3007.filemail.com/api/file/get?filekey=zheRXlm40fnxf_LBk6l6NlwkQJiFDyevuZ_m13GVaVlwwAgPcIsmc9rwOhqFcGUqy2cbfZegyw&pk_vid=2312c3fb86b2e947173315417796c326'; // Replace with your PDF file's URL
+    var pdfUrl = 'https://p-def5.pcloud.com/D4ZfEpaVGZNdkULh7ZZZQsEHXkZ2ZZNsHZkZxQU0Z08ZDYZXQZ5Ns05ZCQUz3eCnHChki46tTAtuuhjIwJOy/Niraj%20Certificate.pdf'; // Replace with your PDF file's URL
 
     // Trigger the download
     var link = document.createElement('a');
